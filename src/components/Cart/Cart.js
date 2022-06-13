5682922bdd14929fed9ef63a1b1b1e7132fb2828
lobos182 <lobos182@gmail.com>
2022-06-13 01:24:46 -0300

    Cart View

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,37 +1,85 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
+import { Link } from 'react-router-dom';
 import CartContext from '../../context/CartContext'
+import "./Cart.css"
 
 const Cart = () => {
 
     const { cart, removeItem, removeAllItem } = useContext(CartContext)
 
+    useEffect(() => {
+        document.querySelectorAll('.button').forEach(button => button.addEventListener('click', e => {
+            if (!button.classList.contains('delete')) {
+                button.classList.add('delete');
+                setTimeout(() => button.classList.remove('delete'), 3200);
+            }
+            e.preventDefault();
+        }));
+    })
+
 
     return (
-        <div>
-            <h1>Carrito</h1>
-            <div>
-                <div>
-                <button onClick={removeAllItem}>borrar carrito</button>
-                </div>
-                {cart.map(prod => {
-                    return (
-                        <div>
-                            <div key={prod.id} style={{ display: 'flex' }}>
-                                <div>{prod.nombre}</div>
-                                <div>Cantidad: {prod.count}</div>
-                                <div>PrecioxUnidad: ${prod.precio}</div>
-                                <div>subtotal: ${prod.precio * prod.count}</div>
-                                <button onClick={() => removeItem(prod.id)}>X</button>
-                                
-                            </div>
-                        </div>
-                    )
-                }
-                )
-                }
 
-            </div>
-        </div>
+        <div className='cartContainer'>
+            {cart.length === 0 ?
+                <><h1>Carrito Vacio</h1><div>
+                    <Link className='Link' to="/">
+                    <button className='finaliza'>Ir a Productos</button>
+                    </Link>
+                </div></>
+
+
+                :
+                <><h1>Carrito</h1><button class="button" onClick={removeAllItem}>
+                    <div class="trash">
+                        <div class="top">
+                            <div class="paper"></div>
+                        </div>
+                        <div class="box"></div>
+                        <div class="check">
+                            <svg viewBox="0 0 8 6">
+                                <polyline points="1 3.4 2.71428571 5 7 1"></polyline>
+                            </svg>
+                        </div>
+                    </div>
+                    <span>Vaciar carrito</span>
+                </button><table>
+                        <thead>
+                            <tr>
+                                <th>Producto</th>
+                                <th>cantidad</th>
+                                <th>Precio por unidad</th>
+                                <th>Subtotal</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        {cart.map(prod => {
+                            return (
+
+                                <tbody key={prod.id}>
+                                    <tr>
+                                        <th>{prod.nombre}</th>
+                                        <th>{prod.count}</th>
+                                        <th>$ {prod.precio}</th>
+                                        <th>$ {prod.precio * prod.count}</th>
+                                        <th><section onClick={() => removeItem(prod.id)}>
+                                            <span class="trash2">
+                                                <span></span>
+                                                <i></i>
+                                            </span>
+                                        </section>
+                                        </th>
+                                    </tr>
+                                </tbody>
+                            )
+                        }
+                        )}
+                    </table><div>
+                        <button className='finaliza'>Finalizar compra</button>
+                    </div></>}
+
+
+        </div >
     )
 }
 
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -12,6 +12,8 @@ const NavBar = (props) => {
     const { getQuantity } = useContext(CartContext)
     const count = getQuantity()
 
+    
+
 
     return (
         <div>
@@ -20,17 +22,17 @@ const NavBar = (props) => {
                     <img src='./images/axis3d.jpg' width="100" height="auto" className="imgLogo" alt='logo' />
                 </Link>
                 <h2 className='titulo'>Axis 3D Mendoza</h2>
-                <input type="checkbox" id="toggler" />
+                <input type="checkbox" id="toggler" className="hamburger" />
                 <label for="toggler"><i className="ri-menu-line"></i></label>
                 <div className="menu">
                     <ul className="list">
                         <li><a href="#!">Nosotros</a></li>
                         <li><Link className='Link' to='/categoria/animales'>Animales</Link></li>
                         <li><Link className='Link' to='/categoria/accesorios'>Accesorios</Link></li>
-                        <li><Link className='Link' to='/categoria/figuras'>Figuras</Link></li>
-                        <li><a href="#!">{count > 0 && <CartWidget />}</a></li>
+                        <li><Link className='Link' to='/categoria/figuras'>Figuras</Link></li>                        
                     </ul>
                 </div>
+                <a href="#!">{count > 0 && <CartWidget />}</a>
             </nav>
         </div>
     );
